3431ee2887be4dcd07b1bf59251cd720bc65c861
alyrx <diogofino.df@gmail.com>
2024-11-28 11:24:25 +0000

    Added MySQL database connection

---
+++ assets/db/config.db.php
@@ -0,0 +1,14 @@
+<?php
+
+# Connection credentials
+$username = "root";
+$password = "";
+
+try {
+    # Create the connection
+    $db = new PDO("mysql:host=localhost;dbname=cinema-app", $username, $password);
+    # Connect
+    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    echo "Couldn't connect to the database: " . $e->getMessage();
+}

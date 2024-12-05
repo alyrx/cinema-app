e3d0d5c07ceed373d6355bb23cc28aeaf0696bc5
alyrx <diogofino.df@gmail.com>
2024-12-05 12:47:40 +0000

    Renamed movies.php to index.php & updated app.css

---
+++ index.php
@@ -0,0 +1,30 @@
+<?php 
+require 'assets/db/config.db.php';
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Cinema App</title>
+
+    <link rel="stylesheet" href="assets/css/app.css">
+</head>
+<body class="open-sans-regular">
+    <header>
+        <h1>Cinema</h1>
+    </header>
+    <main>
+        <section>
+            <div class="movie-card">
+                <img src="assets/images/sorri2.webp">
+
+                <h3>Sorri 2</h3>
+                <p>M16 - 210min</p>
+            </div>
+        </section>
+    </main>
+    <footer></footer>
+</body>
+</html>

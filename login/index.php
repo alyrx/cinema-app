7571724b468712a559a47cab23a92cfab7fe1f2e
alyrx <diogofino.df@gmail.com>
2024-12-17 22:07:18 +0000

    Added CSS styling to login & register pages

--- login/index.php
+++ login/index.php
@@ -11,32 +11,38 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $error = login($db, $email, $password);
 }
 ?>
-
 <!DOCTYPE html>
 <html lang="pt">
-
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="shortcut icon" href="https://avatars.githubusercontent.com/u/85791897" type="image/x-icon">
     <title>Login</title>
+
+    <link rel="stylesheet" href="../assets/css/auth.css">
 </head>
-
-<body>
-    <h2>Login</h2>
-    <form action="./index.php" method="POST">
-        <label for="email">Email:</label>
-        <input type="email" name="email" id="email" required><br>
-
-        <label for="password">Palavra-passe:</label>
-        <input type="password" name="password" id="password" required><br>
-
-        <input type="submit" value="Entrar">
-    </form>
-    <p>Don't have an account yet? <a href="../register/">Register</a></p>
-    <?php if (isset($error)) {
-        echo "<p style='color:red;'>$error</p>";
-    } ?>
+<body class="open-sans-regular">
+    <main>
+        <h2 class="orbitron-italic-900">Login</h2>
+        <form action="./index.php" method="POST">
+            <?php if (isset($error)) {
+                echo "<p style='color:red;'>$error</p>";
+            } ?>
+            <div>
+                <label for="email">Email</label>
+                <input type="email" name="email" id="email" required>
+            </div>
+            <div>
+                <label for="password">Palavra-passe</label>
+                <input type="password" name="password" id="password" required>
+            </div>
+            
+            <button type="submit" class="btn-login">
+                <i class="bi bi-box-arrow-in-right" style="font-weight: bold;"></i>
+                <p>Entrar</p>
+            </button>
+            <p id="register-link">Don't have an account yet? <a href="../register/">Register</a></p>
+        </form>
+    </main>
 </body>
-
 </html>
--- register/index.php
+++ register/index.php
@@ -14,33 +14,39 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     login($db, $email, $password);
 }
 ?>
-
 <!DOCTYPE html>
 <html lang="pt">
-
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="shortcut icon" href="https://avatars.githubusercontent.com/u/85791897" type="image/x-icon">
     <title>Login</title>
+
+    <link rel="stylesheet" href="../assets/css/auth.css">
 </head>
+<body class="open-sans-regular">
+    <main>
+        <h2 class="orbitron-italic-900">Register</h2>
+        <form action="./index.php" method="POST">
+            <div>
+                <label for="name">Nome</label>
+                <input type="name" name="name" id="name" required>
+            </div>
+            <div>
+                <label for="email">Email</label>
+                <input type="email" name="email" id="email" required>
+            </div>
+            <div>
+                <label for="password">Palavra-passe</label>
+                <input type="password" name="password" id="password" required>
+            </div>
 
-<body>
-    <h2>Register</h2>
-    <form action="./index.php" method="POST">
-        <label for="name">Nome:</label>
-        <input type="name" name="name" id="name" required><br>
-
-        <label for="email">Email:</label>
-        <input type="email" name="email" id="email" required><br>
-
-        <label for="password">Palavra-passe:</label>
-        <input type="password" name="password" id="password" required><br>
-
-        <input type="submit" value="Entrar">
-    </form>
-    <p>Have an account? <a href="../login/">Login here</a></p>
-
+            <button type="submit" class="btn-login">
+                <i class="bi bi-box-arrow-in-right" style="font-weight: bold;"></i>
+                <p>Entrar</p>
+            </button>
+            <p id="register-link">Have an account? <a href="../login/">Login here</a></p>
+        </form>
+    </main>
 </body>
-
 </html>
